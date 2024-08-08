2edec41ba6def4f69e99ec71fc2d47555f673244
tornike2002 <toko.toka2000@gmail.com>
2024-08-08 17:22:01 +0400

    added title section for carousel

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import carouselData from "../../../data/carousel.json";
+import CategoryRect from "../../assets/HomeAssets/Category Rectangle.png";
 import { Rate } from "antd";
 import "swiper/css";
 type CarouselTypes = {
@@ -9,11 +10,21 @@ type CarouselTypes = {
   id: number;
   title: string;
   firstPrice: number;
+  defValue: number;
 };
 const Carousel = () => {
   return (
     <React.Fragment>
       <div className="mx-[5%]">
+        <div className="flex flex-col gap-3 font-Monseratt pb-5">
+          <div className="flex items-center gap-2">
+            <img src={CategoryRect} alt="Category Rectangle" />
+            <h2 className="text-red-600 text-sm font-bold leading-3 tracking">
+              Today's
+            </h2>
+          </div>
+          <h1 className="text-3xl font-medium tracking-wide">Flash Sales</h1>
+        </div>
         <Swiper
           spaceBetween={15}
           slidesPerView={4}
@@ -28,13 +39,15 @@ const Carousel = () => {
                   alt="image"
                   className="w-64 h-52 object-contain"
                 />
-                <div className="flex flex-col items-start font-Monseratt gap-2">
+                <div className="flex flex-col items-start font-Monseratt gap-2 pt-10">
                   <h1 className=" font-bold text-[14px]">{item.title}</h1>
                   <div className="flex items-center gap-2">
                     <p className="text-red-600 text-xs">${item.price}</p>
-                    <p className="text-xs">${item.firstPrice}</p>
+                    <p className="text-xs line-through opacity-60 text-black text-bold">
+                      ${item.firstPrice}
+                    </p>
                   </div>
-                  <Rate allowHalf defaultValue={1} />
+                  <Rate allowHalf defaultValue={item.defValue} />
                 </div>
               </SwiperSlide>
             );
