24ff16ee06fb52fcde6872e926bbc9269a15a9bd
tornike2002 <toko.toka2000@gmail.com>
2024-08-08 17:35:45 +0400

    wip for now

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -16,7 +16,7 @@ const Carousel = () => {
   return (
     <React.Fragment>
       <div className="mx-[5%]">
-        <div className="flex flex-col gap-3 font-Monseratt pb-5">
+        <div className="flex flex-col gap-3 font-Monseratt">
           <div className="flex items-center gap-2">
             <img src={CategoryRect} alt="Category Rectangle" />
             <h2 className="text-red-600 text-sm font-bold leading-3 tracking">
@@ -26,7 +26,7 @@ const Carousel = () => {
           <h1 className="text-3xl font-medium tracking-wide">Flash Sales</h1>
         </div>
         <Swiper
-          spaceBetween={15}
+          spaceBetween={25}
           slidesPerView={4}
           onSlideChange={() => console.log("slide change")}
           onSwiper={(swiper) => console.log(swiper)}
@@ -37,9 +37,9 @@ const Carousel = () => {
                 <img
                   src={item.image}
                   alt="image"
-                  className="w-64 h-52 object-contain"
+                  className="w-full sm:h-[250px] sm:w-[250px] object-contain"
                 />
-                <div className="flex flex-col items-start font-Monseratt gap-2 pt-10">
+                <div className="hidden flex-col items-start font-Monseratt gap-2 md:pt-5  sm:flex">
                   <h1 className=" font-bold text-[14px]">{item.title}</h1>
                   <div className="flex items-center gap-2">
                     <p className="text-red-600 text-xs">${item.price}</p>
