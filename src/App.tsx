d1cb958711cfe228b45fc1e2ae003e529c323246
tornike2002 <toko.toka2000@gmail.com>
2024-08-13 16:06:12 +0400

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-
 import { useState } from "react";
 import Header from "./components/Header";
 import BurgerMenu from "./components/BurgerMenu";
 import Overlay from "./components/Overlay";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
   const [isBurgerOpen, setIsBurgerOpen] = useState(false);
   const [isOverlayOpen, setIsOverlayOpen] = useState(false);
 
   return (
+    <>
+      <div className="min-h-screen min-w-screen">
+        <Header
+          isBurgerOpen={isBurgerOpen}
+          setIsBurgerOpen={setIsBurgerOpen}
+          isOverlayOpen={isOverlayOpen}
+          setIsOverlayOpen={setIsOverlayOpen}
+        />
+        <BurgerMenu isBurgerOpen={isBurgerOpen} />
 
-    <div className="min-h-screen min-w-screen">
-      <Header
-        isBurgerOpen={isBurgerOpen}
-        setIsBurgerOpen={setIsBurgerOpen}
-        isOverlayOpen={isOverlayOpen}
-        setIsOverlayOpen={setIsOverlayOpen}
-      />
-      <BurgerMenu isBurgerOpen={isBurgerOpen} />
-
-      <Overlay
-        isOverlayOpen={isOverlayOpen}
-        setIsOverlayOpen={setIsOverlayOpen}
-        setIsBurgerOpen={setIsBurgerOpen}
-      />
-    </div>
+        <Overlay
+          isOverlayOpen={isOverlayOpen}
+          setIsOverlayOpen={setIsOverlayOpen}
+          setIsBurgerOpen={setIsBurgerOpen}
+        />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
